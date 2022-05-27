8669e4dc3cbf1b02e98fa6d6e4a646a174a592a3
JeremyM45 <jeremyvs1945@gmail.com>
2022-05-27 12:41:17 -0700

    add display with links to details on splash page

--- Factory/Controllers/HomeControllers.cs
+++ Factory/Controllers/HomeControllers.cs
@@ -1,12 +1,24 @@
+using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Mvc;
+using Factory.Models;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc.Rendering;
 namespace Factory.Controllers
 {
   public class HomeController : Controller
   {
+    private readonly FactoryContext _db;
+    public HomeController(FactoryContext db)
+    {
+      _db = db;
+    }
     [HttpGet("/")]
     public ActionResult Index()
     {
-      return View();
+      List<Machine> model = _db.Machines.ToList();
+      ViewBag.Engineers = _db.Engineers.ToList();
+      return View(model);
     }
   }
 }
